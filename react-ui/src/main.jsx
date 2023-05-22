28131bf230a9d6f813b8738de76b52452fb46ca5
John Garcia <98444386+jhongarcian@users.noreply.github.com>
2023-05-21 21:41:21 -0500

    call dog ceo api and handle the response

--- react-ui/src/main.jsx
+++ react-ui/src/main.jsx
@@ -3,10 +3,14 @@ import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import store from './app/store'
+import { fetchBreeds } from './features/breedsSlice';
+
 import App from './App';
 
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
+
+store.dispatch(fetchBreeds());
 
 root.render(
   <StrictMode>
