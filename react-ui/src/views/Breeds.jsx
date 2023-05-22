0cce6606688acf18ce732e5d64ccd5542a580c4f
John Garcia <98444386+jhongarcian@users.noreply.github.com>
2023-05-21 21:52:17 -0500

    show breeds at /breeds

--- react-ui/src/views/Breeds.jsx
+++ react-ui/src/views/Breeds.jsx
@@ -1,7 +1,21 @@
 import React from 'react'
+import { useSelector } from 'react-redux';
+import { selectBreeds } from '../features/breedsSlice';
 
 const Breeds = () => {
-  return <div>Breeds</div>
+
+  const data = useSelector(selectBreeds);
+
+  const breeds = data && data.map(breed => {
+    return <li>{breed}</li>
+  })
+
+  return (
+    <>
+      <div>Breeds</div>
+      <ul>{breeds}</ul>
+    </>
+    )
 }
 
 export default Breeds;
