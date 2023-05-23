348fce61a58db1fc541987de6c1b228a9891c318
John Garcia <98444386+jhongarcian@users.noreply.github.com>
2023-05-22 20:23:29 -0500

    toggle selected breeds

--- react-ui/src/views/Breeds.jsx
+++ react-ui/src/views/Breeds.jsx
@@ -1,19 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useSelector } from 'react-redux';
 import { selectBreeds } from '../features/breedsSlice';
 
 const Breeds = () => {
+  
+  const [selectedBreeds, setSelectedBreeds] = useState([])
 
   const data = useSelector(selectBreeds);
 
+  const handleClick = (e) => {
+    const value = e.target.dataset.breed;
+    let _selectedBreeds = []
+
+    if(selectedBreeds.indexOf(value) > -1) {
+      _selectedBreeds = selectedBreeds.filter(breed => breed !== value)
+      setSelectedBreeds(_selectedBreeds)
+    } else {
+      _selectedBreeds = [...selectedBreeds, value];
+    }
+    setSelectedBreeds(_selectedBreeds)
+  };
+
   const breeds = data && data.map(breed => {
-    return <li>{breed}</li>
+    return <li data-breed={breed} key={breed} onClick={handleClick} >{breed}</li>
   })
 
   return (
     <>
       <div>Breeds</div>
       <ul>{breeds}</ul>
+      {JSON.stringify(selectedBreeds)}
     </>
     )
 }
