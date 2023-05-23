0870c8ba68af44ce9eefa7c3ac31458c384a9176
John Garcia <98444386+jhongarcian@users.noreply.github.com>
2023-05-22 21:36:46 -0500

    get favorite breeds in favorite view

--- react-ui/src/views/Favorites.jsx
+++ react-ui/src/views/Favorites.jsx
@@ -1,7 +1,26 @@
 import React from 'react'
+import { useSelector } from "react-redux";
+import { selectSelectedBreeds } from '../features/selectedBreedsSlice';
 
 const Favorites = () => {
-  return <div>Favorites</div>
+  const data = useSelector(selectSelectedBreeds);
+
+  const selecedBreeds = data && data.map(breed => {
+    return (
+      <li
+        data-breed={breed}
+        key={breed}
+      >
+        {breed}
+      </li>
+    )
+  })
+  return (
+    <>
+      <h1>Favorites</h1>
+      <div>{selecedBreeds}</div>
+    </>
+  )
 }
 
 export default Favorites;
