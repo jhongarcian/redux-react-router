7a98b46578a8f622843c056656f5cbeceb80212f
John Garcia <98444386+jhongarcian@users.noreply.github.com>
2023-05-22 21:03:03 -0500

    add button to handle selected breeds

--- react-ui/src/views/Breeds.jsx
+++ react-ui/src/views/Breeds.jsx
@@ -1,10 +1,9 @@
-import React, { useState } from 'react'
-import { useSelector } from 'react-redux';
-import { selectBreeds } from '../features/breedsSlice';
+import React, { useState } from "react";
+import { useSelector } from "react-redux";
+import { selectBreeds } from "../features/breedsSlice";
 
 const Breeds = () => {
-  
-  const [selectedBreeds, setSelectedBreeds] = useState([])
+  const [selectedBreeds, setSelectedBreeds] = useState([]);
 
   const data = useSelector(selectBreeds);
 
@@ -12,8 +11,8 @@ const Breeds = () => {
     const value = e.target.dataset.breed;
     let _selectedBreeds = [];
 
-    if(selectedBreeds.indexOf(value) > -1) {
-      _selectedBreeds = selectedBreeds.filter(breed => breed !== value)
+    if (selectedBreeds.indexOf(value) > -1) {
+      _selectedBreeds = selectedBreeds.filter((breed) => breed !== value);
       setSelectedBreeds(_selectedBreeds);
     } else {
       _selectedBreeds = [...selectedBreeds, value];
@@ -21,26 +20,38 @@ const Breeds = () => {
     setSelectedBreeds(_selectedBreeds);
   };
 
-  const breeds = data && data.map(breed => {
-    return (
-      <li 
-        className={selectedBreeds.indexOf(breed) > -1 ? 'active' : ''}
-        data-breed={breed} 
-        key={breed} 
-        onClick={handleClick} 
-      >
-        {breed}
-      </li>
-      )
-  })
+  const handleSelectedBreeds = (e) => {
+    console.log(selectedBreeds)
+  };
+
+  const breeds =
+    data &&
+    data.map((breed) => {
+      return (
+        <li
+          className={selectedBreeds.indexOf(breed) > -1 ? "active" : ""}
+          data-breed={breed}
+          key={breed}
+          onClick={handleClick}
+        >
+          {breed}
+        </li>
+      );
+    });
 
   return (
     <>
-      <div>Breeds</div>
-      <ul className='breeds' >{breeds}</ul>
-      {JSON.stringify(selectedBreeds)}
+      <div className="breeds-subheader">
+        <h1>Breeds</h1>
+        {selectedBreeds.length > 0 && (
+          <button onClick={handleSelectedBreeds}>
+            Add selections to favorites
+          </button>
+        )}
+      </div>
+      <ul className="breeds">{breeds}</ul>
     </>
-    )
-}
+  );
+};
 
 export default Breeds;
